facf62c9e4af7ab291ff40766ea815b4ccc1fedc
Anthony <anthonyvovo123@gmail.com>
2022-03-07 14:04:42 -0800

    3/7/22 done

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,15 @@
 def caesar_shift! (char, low_range, high_range, shift_factor)
-    # Get current ascii
     current_ascii = char.ord
 
-    if current_ascii < low_range or current_ascii > high_range 
+    # Filter out space and non-alphabetical characters
+    if not current_ascii.between?(low_range,high_range)
         return char
     end
 
     # Shift
     new_ascii = current_ascii + shift_factor
 
-    # Check if new ascii num is > high_range, if yes, then subtract 26 (amount of letters in alphabet) to get new ascii (loop around)
+    # Check if new ascii num is > high_range then subtract 26 (amount of letters in alphabet) to get new ascii (loop around)
     if new_ascii > high_range
         new_ascii -= 26
     end
@@ -21,10 +21,7 @@ end
 
 def to_caesar(input, shift_factor)
     result = ""
-    
-    # Split input string into array of characters
-    # Check each character to see if uppercase or lowercase
-    # require 'pry-byebug'; binding.pry;
+
     input.each_char { |char|
         # If uppercase, follow ascii for uppercase
        if char == char.upcase
